b23545a3cee8afa426548cd979ed872fb76c910d
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-18 15:41:37 +0100

    I numeri generati sono ora casuali

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -1,21 +1,26 @@
 package org.generation.italy.cicli;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class StampaNumeriDispari {
 
 	public static void main(String[] args) {
 		
-		// init Scanner
+		// Scanner init
 		System.out.print("Quanti numeri vuoi generare? ");
 		Scanner scanner = new Scanner(System.in);
 		int howMany = scanner.nextInt();
 		
+		// Random init
+		Random rGen = new Random();
+		
 		// genera array
-		System.out.println("Ecco " + howMany + " numeri casuali:");
+		System.out.println("Ecco " + howMany + " numeri casuali da 0 a 99:");
 		int[] numbers = new int[howMany];
-		for (int i = 0; i < numbers.length; i++) {
-			numbers[i] = i + 1;
+		for(int i = 0; i < numbers.length; i++) {
+			int rNum = rGen.nextInt(100);
+			numbers[i] = rNum;
 			System.out.println(numbers[i]);
 		}
 		
