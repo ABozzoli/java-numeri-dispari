b56564a36c719737bb63b2e343da8f70e89de3fe
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-18 16:03:53 +0100

    Stampa solo numeri dispari

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -18,10 +18,18 @@ public class StampaNumeriDispari {
 		// genera array
 		System.out.println("Ecco " + howMany + " numeri casuali da 0 a 99:");
 		int[] numbers = new int[howMany];
+		
 		for(int i = 0; i < numbers.length; i++) {
+			
+			// assegna ad ogni elemento dell'array un numero casuale
 			int rNum = rGen.nextInt(100);
 			numbers[i] = rNum;
-			System.out.println(numbers[i]);
+			
+			// lo stampa solo se dispari
+			if (!(rNum % 2 == 0)) {
+				System.out.println(numbers[i]);
+			}
+			
 		}
 		
 		// Scanner close
